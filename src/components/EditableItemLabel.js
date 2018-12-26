bcb37d67bdc134809172f6f1fab5941dc58092ad
sg3957 <sg3957@nyu.edu>
2018-12-25 23:21:17 -0500

    added in-line editing for list titles. BUGS needed to be fixed: adds list title to bottom of list as an item following PATCH and does not account for last character in edit field

--- src/components/EditableItemLabel.js
+++ src/components/EditableItemLabel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class EditableLabel extends React.Component {
+export default class EditableItemLabel extends React.Component {
     constructor(props) {
         super(props);
 
--- src/components/ListDetails.js
+++ src/components/ListDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ItemForm from './ItemForm'
-import EditableLabel from './EditableLabel'
+import EditableItemLabel from './EditableItemLabel'
+import EditableTitleLabel from './EditableTitleLabel'
 //import '../App.css';
 
 
@@ -9,11 +10,26 @@ class ListDetails extends Component {
     return (
       <div id="detail-container">
         <h1>List Details</h1>
-        <h3>{this.props.clickedListId ? this.props.renderList.name : null}</h3>
+        <h3>{this.props.clickedListId ? <EditableTitleLabel
+                  clickedListId={this.props.clickedListId}
+                  id={this.props.clickedListId}
+                  key={this.props.id}
+                  text={this.props.renderList.name}
+                  labelClassName='myLabelClass'
+                  inputClassName='myInputClass'
+                  inputWidth='200px'
+                  inputHeight='25px'
+                  inputMaxLength='50'
+                  labelFontWeight='bold'
+                  inputFontWeight='bold'
+                  onFocus={this._handleFocus}
+                  onFocusOut={this._handleFocusOut}
+              />
+           : null}</h3>
         {this.props.clickedListId ? <ItemForm clickedListId={this.props.clickedListId} addItem={this.props.addItem}/> : null}
         <ul>{this.props.clickedListId ? this.props.renderList.items.map(i=>
           // <li key={i.id}>{i.name}</li>
-          <EditableLabel
+          <EditableItemLabel
                     clickedListId={this.props.clickedListId}
                     id={i.id}
                     key={i.id}
